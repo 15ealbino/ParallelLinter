d63051dea235e99ac7f36a31160fe039d5dca4dd
Chris Jantzen <jantzeca@mail.uc.edu>
2019-11-21 16:36:47 -0500

    Updates

--- SequentialLinter/run.py
+++ SequentialLinter/run.py
@@ -1,10 +1,12 @@
 # Sequential version
 import glob
-from sequentialLinter import checkFile as run
+from sequentialLinter import run
 
-directory = input("Enter your directory: ")
+# directory = input("Enter your directory: ")
 lstOfFiles = []
-lstOfFiles.append()
+lstOfFiles.append(
+    "/Users/chrisjantzen/school/Fall2019/Parallel/finalProject/ParallelLinter/SequentialLinter/testFile.js"
+)
 for i in lstOfFiles:
     error = run(i)
     print(error)
--- SequentialLinter/sequentialLinter.py
+++ SequentialLinter/sequentialLinter.py
@@ -1,30 +1,33 @@
 # based on https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-python/
 def checkFile(jsFile):
-    open_list = ["[","{","("] 
-    close_list = ["]","}",")"] 
+    open_list = ["[", "{", "("]
+    close_list = ["]", "}", ")"]
 
+    newLineCount = 0
     stack = []
     for i in jsFile:
+        if i == "\n":
+            newLineCount += 1
         if i in open_list:
             stack.append(i)
         elif i in close_list:
             pos = close_list.index(i)
-            if((len(stack) > 0) and (open_list[pos] == stack[len(stack)-1])):
+            if (len(stack) > 0) and (open_list[pos] == stack[len(stack) - 1]):
                 stack.pop()
             else:
-                return("Unbalanced at line")
+                return "Unbalanced at line {}".format(newLineCount)
     if len(stack) == 0:
-        return ("File is all good")
-        
+        print(newLineCount)
+        return "File is all good"
 
-def run(jsfile): 
+
+def run(jsfile):
     s = ""
     with open(jsfile) as fp:
         line = fp.readline()
         s += line
         while line:
             line = fp.readline()
-            s+= line
-    checkFile(s)
-    
-    
+            s += line
+    return checkFile(s)
+
