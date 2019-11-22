5decc8cfbf6fd34b0804be12261177ba40499068
Chris Jantzen <jantzeca@mail.uc.edu>
2019-11-21 19:27:15 -0500

    format

--- SequentialLinter/run.py
+++ SequentialLinter/run.py
@@ -2,8 +2,8 @@
 import glob
 from sequentialLinter import run
 
-directory = input('Enter your directory: ')
-lstOfFiles = glob.glob(directory +"*.js")
+directory = input("Enter your directory: ")
+lstOfFiles = glob.glob(directory + "*.js")
 for i in lstOfFiles:
     error = run(i)
     print(error)
