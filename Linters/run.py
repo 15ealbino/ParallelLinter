64ab5b7192a65cca0522586f36ce8f4c70fe5bad
Chris Jantzen <jantzeca@mail.uc.edu>
2019-12-05 20:43:22 -0500

    move plotting to it's own file

--- Linters/run.py
+++ Linters/run.py
@@ -2,9 +2,8 @@
 import glob
 from sequentialLinter import run as srun
 from ParallelLinter import run as prun
+from plotData import plot_data
 from timeit import default_timer as timer
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 directory = input("Enter your directory: ")
@@ -28,49 +27,4 @@ for i in lstOfFiles:
     Pdone = timer() - startP
     pTimes.append(Pdone)
 
-
-labels = [f"File {i}" for i in range(len(lstOfFiles))]
-xaxis = np.arange(len(lstOfFiles))
-width = 0.35
-
-fig, ax = plt.subplots(figsize=(10, 7))
-rects1 = ax.bar(
-    xaxis - width / 2,
-    [round(i, 4) for i in sTimes],
-    width,
-    label="Sequential",
-    color="#73d219",
-)
-rects2 = ax.bar(
-    xaxis + width / 2,
-    [round(i, 4) for i in pTimes],
-    width,
-    label="Parallel",
-    color="#3378f0",
-)
-
-ax.set_ylabel("Times")
-ax.set_title("Sequential vs. Parallel")
-ax.set_xticks(xaxis)
-ax.set_xticklabels(labels)
-ax.legend()
-
-
-def autolabel(rects):
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate(
-            "{}".format(height),
-            xy=(rect.get_x() + rect.get_width() / 2, height),
-            xytext=(0, 3),  # 3 points vertical offset
-            textcoords="offset points",
-            ha="center",
-            va="bottom",
-        )
-
-
-autolabel(rects1)
-autolabel(rects2)
-fig.tight_layout()
-
-plt.show()
+plot_data(len(lstOfFiles), sTimes, pTimes)
